d3b6f0a1e26ed2c72e37af1782bb93c3718a148d
Graham Mosley <gmosley@seas.upenn.edu>
2017-12-04 21:47:08 -0500

    parsing updates

--- parse_flawfinder.py
+++ parse_flawfinder.py
@@ -1,9 +1,9 @@
 import re
 
-regex = re.compile(r"([^:]*):(\d+):(\d*): {2}([^:]*):(.*)")
+regex = re.compile(r"([^:]*):(\d+):(\d*): {2}(\[\d\])([^:]*):(.*)")
 
 with open('results/flawfinder.txt', 'r') as f:
     for line in f.readlines():
         line = line.rstrip()
         m = regex.match(line)
-        filename, lineno, colno, category, description = m.groups()
+        filename, lineno, colno, category, check_name, description = m.groups()
